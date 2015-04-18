1fcd147cbc807061d023a642542bbbef53fa7cc6
bartv <bart.veldstra@engine-software.nl>
2015-04-18 13:59:03 +0200

    Reverted project to base state

--- src/Main.hx
+++ src/Main.hx
@@ -12,11 +12,6 @@ class Main extends Engine
 		HXP.console.enable();
 #end
 		HXP.scene = new MainScene();
-
-        Input.define("up", [Key.UP, Key.W]);
-        Input.define("down", [Key.DOWN, Key.S]);
-        Input.define("left", [Key.LEFT, Key.A]);
-        Input.define("right", [Key.RIGHT, Key.D]);
 	}
 
 	public static function main() { new Main(); }
--- src/MainScene.hx
+++ src/MainScene.hx
@@ -2,15 +2,7 @@ import com.haxepunk.Scene;
 
 class MainScene extends Scene
 {
-    var player:Player;
-    var cat:Cat;
-
 	public override function begin()
 	{
-        player = new Player(100, 100);
-        add(player);
-
-        cat = new Cat(player);
-        add(cat);
 	}
 }
